edb61639fdcf8b914e8bc6e6797aa5dd2c4da2b3
mohammad abusalim <mohammedabusalim05@gmail.com>
2026-09-14 21:44:16 +0300

    Enable CORS for the Flask app

--- service/__init__.py
+++ service/__init__.py
@@ -2,8 +2,10 @@
 Service Package
 """
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # This must be imported after the Flask app is created
 from service import routes
@@ -12,5 +14,5 @@ from service.common import log_handlers
 log_handlers.init_logging(app, "gunicorn.error")
 
 app.logger.info(70 * "*")
-app.logger.info("  S E R V I C E   R U N N I N G  ".center(70, "*"))
+app.logger.info(" S E R V I C E   R U N N I N G ".center(70, "*"))
 app.logger.info(70 * "*")
